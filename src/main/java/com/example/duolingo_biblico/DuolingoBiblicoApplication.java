ed81d2bc7797a6d1fbf52a50e0811cea721046b5
FellipeOliveiraBartalini <fellipe.oliveira.work@gmail.com>
2025-07-31 11:11:37 -0300

    git init

---
+++ src/main/java/com/example/duolingo_biblico/DuolingoBiblicoApplication.java
@@ -0,0 +1,13 @@
+package com.example.duolingo_biblico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuolingoBiblicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DuolingoBiblicoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/duolingo_biblico/DuolingoBiblicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.duolingo_biblico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DuolingoBiblicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
